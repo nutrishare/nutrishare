1264932862ed7aff471641db36933a59bc16b0e2
Daste <stefankar1000@gmail.com>
2024-01-29 22:53:29 +0100

    Add an empty Expo app to apps/mobile

---
+++ apps/mobile/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello, World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
