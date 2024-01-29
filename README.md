f43d610a822ec52c191f023b049a701152057c45
Daste <stefankar1000@gmail.com>
2024-01-29 22:53:29 +0100

    Add an empty Expo app to apps/mobile

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ We use pnpm for package management and bun as the runtime.
 4. Migrate the database: `pnpm db:migrate`
 5. Run the project: `bun dev`
 6. Open http://localhost:3000/ in your browser
+
+To run the mobile app (Expo), you need to start it separately, as their CLI doesn't fully work when running non-interatively:
+
+`pnpm --filter mobile start --tunnel`
+
---
+++ apps/mobile/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello, World! :^)</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+  },
+});
