316225b8f7dc6e6adb4e4665057a6da8f60673d3
Daste <stefankar1000@gmail.com>
2024-01-30 21:32:37 +0100

    Add very basic barcode scanner with the expo-camera API

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,11 +1,103 @@
+import { CameraView, useCameraPermissions } from "expo-camera/next";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Platform,
+  Modal,
+} from "react-native";
 
 export default function App() {
+  if (Platform.OS === "android") {
+    return AndroidCodeScanner();
+  } else if (Platform.OS === "ios") {
+    return IOSCodeScanner();
+  }
+}
+
+function AndroidCodeScanner() {
+  useCameraPermissions();
+  const [barcode, setBarcode] = useState(null);
+  const [scannerActive, setScannerActive] = useState(false);
+
+  const onBarcodeScanned = (result) => {
+    setBarcode(result.data);
+    setScannerActive(false);
+  };
+
+  const launchScanner = () => {
+    setScannerActive(true);
+  };
+
+  if (scannerActive) {
+    return (
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={scannerActive}
+        onRequestClose={() => setScannerActive(false)}
+      >
+        <CameraView
+          style={{ flex: 1, width: "100%" }}
+          facing="back"
+          barcodeScannerSettings={{
+            barCodeTypes: ["qr", "upc_a", "upc_e", "ean13", "ean8"],
+          }}
+          onBarcodeScanned={onBarcodeScanned}
+        />
+      </Modal>
+    );
+  }
+
   return (
     <View style={styles.container}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <TouchableOpacity style={styles.button} onPress={launchScanner}>
+          <Text style={styles.text}>Scan Barcode</Text>
+        </TouchableOpacity>
+      </View>
+      {barcode && <Text style={{ flex: 1, color: "black" }}>{barcode}</Text>}
+
       <StatusBar style="auto" />
-      <Text style={styles.text}>Hello, World! :^)</Text>
+    </View>
+  );
+}
+
+function IOSCodeScanner() {
+  const [barcode, setBarcode] = useState(null);
+
+  const onBarcodeScanned = async (result) => {
+    setBarcode(result.data);
+    await CameraView.dismissScanner();
+  };
+
+  useEffect(() => {
+    const subscription = CameraView.onModernBarcodeScanned(onBarcodeScanned);
+    return subscription.remove;
+  });
+
+  const launchScanner = async () => {
+    await CameraView.launchModernScanner({
+      barCodeTypes: ["upca", "upce", "ean13", "ean8", "qr"],
+      isGuidanceEnabled: true,
+      isHighlightingEnabled: true,
+      isPinchToZoomEnabled: true,
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <TouchableOpacity style={styles.button} onPress={launchScanner}>
+          <Text style={styles.text}>Scan Barcode</Text>
+        </TouchableOpacity>
+      </View>
+      {barcode && <Text style={{ flex: 1, color: "black" }}>{barcode}</Text>}
+
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -18,6 +110,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   text: {
-    fontSize: 30,
+    flex: 1,
+    color: "white",
+    textAlign: "center",
+  },
+  button: {
+    borderRadius: 4,
+    padding: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue",
   },
 });
